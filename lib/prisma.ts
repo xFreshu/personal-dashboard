f6510ef19ed88d89c000134c9591f9789d387a70
xFreshu <lprzybyszcontact@gmail.com>
2026-04-21 20:10:51 +0200

    feat: migrate Prisma to use pg adapter and update configuration for library engine type

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -6,13 +12,7 @@ const globalForPrisma = globalThis as unknown as {
 
 const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+  new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
